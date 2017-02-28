f291346a1c875d6464e345593fef3ffee7222899
Tymoteusz Buczkowski <moyubori@gmail.com>
2017-02-28 11:05:01 +0100

    Removed unnecessary files. Created LocalPlayer and RemotePlayer prefabs.

---
+++ Assets/Resources/Scripts/Server.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class responsible for communication with server
+public class Server : MonoBehaviour {
+
+
+
+}
